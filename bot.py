4f5937e4dd1957fbdc16b413859057250335440d
Asus <Asus@DESKTOP-QEM1JBE>
2021-02-13 13:15:24 +0100

    update file bot.py, new organization and create a api function

--- bot.py
+++ bot.py
@@ -1,2 +1,31 @@
 import tweepy
+import os 
+
+def create_bot_api():
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    #Create an API object
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+        print("API Created")
+    except:
+        print("Error during API creation")
+    return api
+
+def main():
+    api = create_bot_api()
+    api.update_status("Tamos activos")
+    
+if __name__=="__main__":
+    main()
+
+
 
